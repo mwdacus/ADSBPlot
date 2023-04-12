e755cec0b879e74ab7ddd92794c5c6791a2445d6
Michael Dacus <mwdacus@gmail.com>
2023-04-12 15:40:46 -0700

    create mat file

---
+++ util/FormStruct.m
@@ -0,0 +1,36 @@
+%% Code Information
+%*************************************************************************
+%Stanford GPS
+
+%Creates a .mat file using data from UI (MAIN_EventUpload) 
+
+%*************************************************************************
+
+function [pkg]=FormStruct(app)
+        %Define main struct
+        %Define airport struct
+        location=struct('lat',app.Lat.Value,'lon',app.Lon.Value,'alt',app.Alt.Value);
+        airport=struct('icao',app.AirportICAO.Value,'Name',app.NearestAirportName.Value,...
+            'Location',location);
+        %Read ADS-B Data to populate struct
+        starttime=min(app.adsbdata.time);
+        endtime=max(app.adsbdata.time);
+        aircraft={unique(app.adsbdata.icao)};
+        boxlat=[min(app.adsbdata.lat) max(app.adsbdata.lat)];
+        boxlon=[min(app.adsbdata.lon) max(app.adsbdata.lon)];
+        boxalt=[min(app.adsbdata.alt) max(app.adsbdata.alt)];
+        %Create East-North-Up Table, combine with original adsbdata
+        [enudata]=ENUData(app.adsbdata.lat,app.adsbdata.lon,app.adsbdata.alt,...
+            [location.lat,location.lon,location.alt]);
+        app.adsbdata=[app.adsbdata enudata];
+        %Get Elevation Data
+        [elevdata,geodata]=GetElevationData(app.boxlat,app.boxlon,app.UsernameEditField.Value,...
+            app.PasswordEditField.Value);
+        %Define struct
+        app.eventdata=struct('Airport_Info',airport,'Start_Date',...
+            app.StartInterference.Value,'End_Date',app.EndInterference.Value,...
+            'StartTime',starttime,'EndTime',endtime,'Aircraft',aircraft,...
+            'boxlat',boxlat,'boxlon',boxlon,'boxalt',boxalt,...
+            'adsbdata',app.adsbdata,'Z',elevdata,'RZ',geodata);
+        pkg=app.eventdata;
+end
